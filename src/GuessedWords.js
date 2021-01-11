73697e0d0eaa819adff4e84a045eb84f32a56d81
Hubert Luc <hubert.luc@limango.pl>
2021-01-11 10:24:45 +0100

    start tests for GuessedWords

---
+++ src/GuessedWords.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes, { string, number } from "prop-types";
+
+const GuessedWords = (props) => {
+  let contents;
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test="guessed-instructions">
+        Try to guess the secret word!
+      </span>
+    );
+  }
+  return <div data-test="component-guessed-words">{contents}</div>;
+};
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default GuessedWords;
