b8510bb134b7892dd855a4212c58b642566678f2
Hubert Luc <hubert.luc@limango.pl>
2021-01-14 07:57:46 +0100

    add styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React, { Component } from "react";
 import "./App.css";
 import GuessedWords from "./GuessedWords";
+import Congrats from "./Congrats";
 
-function App() {
-  return <GuessedWords guessedWords={[]} />;
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
